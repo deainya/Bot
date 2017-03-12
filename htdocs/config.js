ad42b0ab88b79486ae4b91c966963a839962daad
deainya <deainya@gmail.com>
2017-03-12 10:26:47 +0400

    Расписание на чт.

--- htdocs/config.js
+++ htdocs/config.js
@@ -5,6 +5,5 @@
  *  For a description of the Bot API, see this page: https://core.telegram.org/bots/api
  */
 module.exports = {
-  //port: process.env.port || 5000,
-  bot_token: '207238769:AAGUdhWNUXeoTHriijyS1YtqsQN5WC2JmAk' //process.env.BOT_TOKEN ||
+  bot_token: '207238769:AAGUdhWNUXeoTHriijyS1YtqsQN5WC2JmAk' //||process.env.BOT_TOKEN
 }
